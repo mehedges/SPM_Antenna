72ee26327ec3280c683abe422425b1529b30b461
Madi Hedges <102606124+mehedges@users.noreply.github.com>
2024-09-20 17:19:31 -0700

    Update madi time.py

--- madi time.py
+++ madi time.py
@@ -1,3 +1,20 @@
+#-----------------VARIABLES--------------------
 H = ''       #magnetic field intesity in amperes/meter
 D = ''       #displacement flux density in coulombs/meters squared
 J = ''       #current density in amperes/meters squared
+E = ''       #electric field intensity in volts/meter
+dl = ''      # infinitesimal dipole antenna with length dl
+ω = ''       #frequency in rad/seconds
+t = ''       #time in seconds
+λ = ''       #wavelength in meters
+ϵ = ''       #permittivity of the material surrounding the antenna in Farads/meter
+r = ''       #spherical coordinate r
+θ = ''       #spherical coordinate theta
+ϕ = ''       #spherical coordinate phi
+ 
+#-----------------EQUATIONS--------------------
+sin current = I*cos(ωt)                         #sinusoidal current
+P_receiver = P_transmitter * (A/(4*pi*r)^2)     #power of receiver 
+near_field = 
+far_field = 
+
