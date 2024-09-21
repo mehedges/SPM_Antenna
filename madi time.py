b4c3b8d5a14060f6eac9e124eab7fad01a42e91c
Madi Hedges <102606124+mehedges@users.noreply.github.com>
2024-09-20 17:36:00 -0700

    Update madi time.py

--- madi time.py
+++ madi time.py
@@ -11,10 +11,16 @@ t = ''       #time in seconds
 r = ''       #spherical coordinate r
 θ = ''       #spherical coordinate theta
 ϕ = ''       #spherical coordinate phi
- 
+n = ''       #radiation efficiency in decibels
+μ = ''       #usually has a specific value depending on material
+σ = ''       #usually has a specific value depending on material
+Z = 377      #in ohms, the impedance of free space
+
 #-----------------EQUATIONS--------------------
 sin current = I*cos(ωt)                         #sinusoidal current
 P_receiver = P_transmitter * (A/(4*pi*r)^2)     #power of receiver 
+R_rad = ((2*pi)/3)*Z*((d^2)/(λ^2))              #Radiation resistance
+R_ohmic = sqrt((pi*f*μ)/σ)*(d/(2*pi*a))         #resistance of a metal wire at high frequency
 near_field = 
 far_field = 
 
